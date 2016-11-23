4b1b4006f6d597c2d93261c824eab612bd17047d
jvoiture <meganos@yopmail.com>
2016-11-23 10:39:11 +0000

    Combine

---
+++ combine.php
@@ -0,0 +1,13 @@
+<?php
+	$files=array(
+		'normalize.min.css',
+		'onepcssgrid.css',
+		'main.css',
+	);
+	
+	$out=array();
+	foreach($files as $f){
+		$out[]=file_get_contents($f);
+	}
+	file_put_contents();
+?>
---
+++ run.bat
@@ -0,0 +1,2 @@
+C:\wamp\bin\php\php5.2.8\php -f .\combine.php
+
